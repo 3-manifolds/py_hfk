eee2cfff8557015cc65712b277bf4875e4e11b91
Marc Culler <culler@math.uic.edu>
2014-11-17 16:23:29 -0600

    Fixed divide by zero error for knots with fewer than 5 crossings.

--- hfk_class.cpp
+++ hfk_class.cpp
@@ -440,7 +440,7 @@ int Link::BuildEdges(int MM, const char *msg, int *count) {
   // Add the edges.
   while (end < (int)Graph[MM].size()) {
     end = min(index + step, (int)Graph[MM].size());
-    percent = (*count + index)/(graphsize/50);
+    percent = (*count + index)/(1 + graphsize/50);
     if ( Progress(msg, max(1, percent)) )
       return -1;
     for(; index < end; index++) {
@@ -509,7 +509,7 @@ int Link::Reduce(int MM, const char *msg, int *count){
 
   while (end < (int)Graph[MM].size()) {
     end = min(source + step, (int) Graph[MM].size());
-    percent = (*count + source)/(graphsize/50);
+    percent = (*count + source)/(1 + graphsize/50);
     if ( Progress(msg, max(1,percent)) )
       return -1;
     for(; source<end; source++) {
